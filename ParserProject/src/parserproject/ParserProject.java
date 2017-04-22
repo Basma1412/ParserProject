c91792d0fdcf101e6ff8b55b94ebea5cc45348bf
Basma1412 <13p6036@eng.asu.edu.eg>
2017-04-22 12:20:53 +0200

    Parser Project

---
+++ ParserProject/src/parserproject/ParserProject.java
@@ -0,0 +1,423 @@
+package parserproject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Parts {
+
+    String part1;
+    String part2;
+
+    public Parts(String part1, String part2) {
+        this.part1 = part1;
+        this.part2 = part2;
+    }
+}
+
+class Parser {
+
+    ArrayList<String> tokens;
+    int current_index;
+    String token;
+
+    public Parser(ArrayList<String> tokens) {
+        this.tokens = tokens;
+        this.current_index = 0;
+    }
+
+    public void updateToken() {
+//        current_index++;
+    }
+
+    public String getToken() {
+        if (current_index >= tokens.size()) {
+            return "";
+        }
+//|| tokens.get(current_index).equals(";")
+        return tokens.get(current_index);
+    }
+
+    public boolean factor() {
+
+        if (getToken().equals("(")) {
+            current_index++;
+            if (exp()) {
+//                current_index++;
+                return getToken().equals(")");
+            } else {
+                return false;
+            }
+        } else {
+            return getToken().equals("number") || getToken().equals("identifier");
+        }
+    }
+
+    public boolean mulop() {
+        return getToken().equals("*") || getToken().equals("/");
+    }
+//term->termmulopfactor|factor
+    //term->factor term2
+    //term2->mulopfactor|E
+
+    public boolean term() {
+        boolean flag = false;
+        if (factor()) {
+            flag = true;
+        }
+        if (flag) {
+            current_index++;
+            if (term2()) {
+                return true;
+            } else {
+                current_index--;
+                return true;
+            }
+        } else {
+            return flag;
+        }
+    }
+
+    public boolean ret = false;
+
+    public boolean term2() {
+
+        if (mulop()) {
+            current_index++;
+            if (factor()) {
+                current_index++;
+                ret = true;
+                return term2();
+            } else {
+
+                current_index--;
+
+                return false;
+            }
+        } else {
+//            return getToken().equals("");
+//            if (ret)
+//            {
+            current_index--;
+//            }
+
+            return true;
+        }
+    }
+
+    public boolean addop() {
+
+        return getToken().equals("+") || getToken().equals("-");
+    }
+
+    public boolean simpleExp() {
+        boolean flag = false;
+        if (term()) {
+            flag = true;
+        }
+
+        if (flag) {
+            current_index++;
+            if (simpleExp2()) {
+                return true;
+            } else {
+                current_index--;
+                return true;
+            }
+
+        } else {
+            return false;
+        }
+    }
+
+    //simpleexp->term simpleexp2
+    //simpleexp2->addop simpleexp2|E
+    boolean flagsE = false;
+
+    public boolean simpleExp2() {
+
+        if (addop()) {
+            current_index++;
+            if (term()) {
+                current_index++;
+                flagsE = true;
+                return simpleExp2();
+            } else {
+//                current_index--;
+                return false;
+            }
+        } else {
+//            return getToken().equals("");
+//            if (flagsE)
+//            {
+            current_index--;
+//            }
+            return true;
+        }
+
+    }
+
+    public boolean comparisonOp() {
+        String xy = getToken();
+        return xy.equals("<") || xy.equals("=") || xy.equals(">");
+    }
+
+    //exp-> simplexp x
+    //x-> comp simple|siple
+    public boolean exp() {
+        if (simpleExp()) {
+            current_index++;
+            return exp2();
+        } else {
+            return false;
+        }
+    }
+
+    public boolean exp2() {
+        if (comparisonOp()) {
+            current_index++;
+            return simpleExp();
+        } else {
+            current_index--;
+            return true;
+        }
+    }
+
+    public boolean writeStmt() {
+
+        if (getToken().equals("write")) {
+            current_index++;
+            return exp();
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean readStmt() {
+        if (getToken().equals("read")) {
+            current_index++;
+            return (getToken().equals("identifier"));
+        } else {
+            return false;
+        }
+    }
+
+    public boolean assignStmt() {
+
+        if (getToken().equals("identifier")) {
+            current_index++;
+            if (getToken().equals(":")) {
+                current_index++;
+                if (getToken().equals("=")) {
+                    current_index++;
+                    return exp();
+                } else {
+                    current_index--;
+                }
+            } else {
+                current_index--;
+            }
+        }
+        return false;
+    }
+
+    public boolean repeatStmt() {
+
+        if (getToken().equals("repeat")) {
+            current_index++;
+            if (statementSequence()) {
+                current_index++;
+                if (getToken().equals("until")) {
+                    current_index++;
+                    return exp();
+                }
+            }
+
+        }
+        return false;
+
+    }
+
+    public boolean ifStmt() {
+        if (getToken().equals("if")) {
+            current_index++;
+            if (exp()) {
+                current_index++;
+                if (getToken().equals("then")) {
+                    current_index++;
+                    if (statementSequence()) {
+                        current_index++;
+                        return ifStmt2();
+                    } else {
+                        current_index--;
+                    }
+                }
+            } else {
+                current_index--;
+            }
+
+        }
+        return false;
+
+    }
+
+    public boolean ifStmt2() {
+        switch (getToken()) {
+            case "end":
+                return true;
+            case ";":
+                return true;
+            case "else":
+                current_index++;
+                if (statementSequence()) {
+                    current_index++;
+                    return (getToken().equals("end"));
+                }
+                break;
+        }
+        return false;
+    }
+
+    public boolean statement() {
+        if (ifStmt()) {
+            System.out.println("If Statement found");
+            return true;
+
+        } else if (readStmt()) {
+
+            System.out.println("Read Statement found");
+            return true;
+        } else if (repeatStmt()) {
+
+            System.out.println("Repeat Statement found");
+            return true;
+        } else if (writeStmt()) {
+
+            System.out.println("Write Statement found");
+            return true;
+        } else if (assignStmt()) {
+
+            System.out.println("Assign Statement found");
+//            current_index--;
+            return true;
+        } else {
+            return false;
+        }
+
+//        
+//        return ifStmt() || readStmt() || repeatStmt() || writeStmt() || assignStmt();
+    }
+
+    //statement seq2
+    //seq2 ;statement||E
+    public boolean statementSequence() {
+        if (statement()) {
+            current_index++;
+            return statementSequence2();
+        } else {
+            return false;
+        }
+    }
+
+    public boolean statementSequence2() {
+        if (getToken().equals(";")) {
+            current_index++;
+            return (statement());
+        } else {
+            return statement();
+        }
+    }
+
+    public boolean program() {
+        if (statementSequence()) {
+            System.out.println("Program Found");
+            return true;
+        }
+        return false;
+    }
+
+}
+
+public class ParserProject {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner s = null;
+        ArrayList<String> data = new ArrayList<>();
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("../scanner_output.txt")));
+            while (s.hasNext()) {
+                String inputString = s.next();
+                data.add(inputString);
+            }
+        } catch (Exception e) {
+
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+
+        }
+        ArrayList<Parts> input = new ArrayList<>();
+        int j = 0;
+        for (String data1 : data) {
+
+            String[] parts = data1.split("~", 2);
+            String part1 = parts[0];
+            String part2 = parts[1];
+            input.add(j++, new Parts(part1, part2));
+        }
+
+        ArrayList<String> parsed_data = new ArrayList<>();
+        j = 0;
+        for (Parts input1 : input) {
+            String part1 = input1.part1;
+            String part2 = input1.part2;
+
+            if ("Identifier".equals(part2)) {
+                parsed_data.add(j++, "identifier");
+            } else if ("number".equals(part2)) {
+                parsed_data.add(j++, "number");
+            } else {
+                parsed_data.add(j++, part1);
+            }
+        }
+
+        ArrayList<ArrayList<String>> stats = new ArrayList<>();
+        ArrayList<String> temp = new ArrayList<>();
+        int xx = 0;
+        int xxx = 0;
+
+        for (String input1 : parsed_data) {
+
+            temp.add(xx++, input1);
+            if (input1.equals(";")) {
+                ArrayList<String> temp2 = new ArrayList<>();
+                for (String temp1 : temp) {
+                    temp2.add(temp1);
+                }
+                stats.add(xxx++, temp2);
+                temp.clear();
+                xx = 0;
+            }
+
+        }
+
+        ArrayList<String> program = new ArrayList<>();
+        for (ArrayList<String> input1 : stats) {
+            program.addAll(input1);
+        }
+
+//        for (String x:program)
+//        {
+//            System.out.println(x+ " ");
+//        }
+        Parser current = new Parser(program);
+        current.program();
+    }
+
+}
---
+++ src/compilersproject/CompilersProject.java
@@ -0,0 +1,217 @@
+package compilersproject;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import sun.org.mozilla.javascript.internal.ScriptRuntime;
+
+public class CompilersProject {
+
+    public static String[] reservedWords = {"if", "then", "else", "end", "repeat", "until", "read", "write"};
+    public static String[] specialSymbols = {"+", "_", "-", "*", "/", "=", "<", ">", "(", ")", ";", ":="};
+
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        ArrayList<String> data = new ArrayList<>();
+        ArrayList<String> reserved_words = new ArrayList<>();
+        ArrayList<String> special_symbols = new ArrayList<>();
+        ArrayList<String> content = new ArrayList<>();
+        ArrayList<String> other = new ArrayList<>();
+        ArrayList<String> number = new ArrayList<>();
+        ArrayList<String> identifier = new ArrayList<>();
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("tiny_sample_code.txt")));
+            while (s.hasNext()) {
+                String inputString = s.next();
+                data.add(inputString);
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+
+        int i = 0;
+        boolean comment = false;
+        for (i = 0; i < data.size(); i++) {
+
+            String current = data.get(i);
+            String rest;
+
+            if (current.startsWith("{")) {
+                comment = true;
+            } else if (current.contains("{") && !(current.startsWith("{"))) {
+                comment = true;
+                rest = current.split("\\{")[0];
+                current = rest;
+                
+                content.add(current);
+            }
+
+            if (current.endsWith("}")) {
+                comment = false;
+                continue;
+            }
+
+            if (current.contains("}")) {
+                comment = false;
+                rest = current.substring(data.get(i).indexOf("}") + 1);
+                current = rest;
+            }
+
+            
+            
+            if (!comment) {
+
+                content.add(current);
+
+            }
+
+        }
+
+        boolean character = false;
+        boolean digit = false;
+
+        ArrayList<Character> chars = new ArrayList<>();
+        ArrayList<Character> digits = new ArrayList<>();
+
+        try {
+
+            try (PrintWriter writer = new PrintWriter("scanner_output.txt", "UTF-8")) {
+                for (String other1 : content) {
+                    if (character) {
+                        StringBuilder result = new StringBuilder(chars.size());
+                        for (Character c : chars) {
+                            result.append(c);
+                        }
+                        String output = result.toString();
+                        boolean res = false;
+                        for (i = 0; i < 8; i++) {
+                            if (output.equals(reservedWords[i])) {
+                                res = true;
+                            }
+                        }
+                        if (res) {
+                            writer.println(output + "~Reserved_Word");
+                        } else {
+                            writer.println(output + "~Identifier");
+                        }
+                        identifier.add(output);
+                        chars.clear();
+                        character = false;
+                    }
+                    
+                    if (digit) {
+                        StringBuilder result = new StringBuilder(digits.size());
+                        for (Character c : digits) {
+                            result.append(c);
+                        }
+                        String output = result.toString();
+                        number.add(output);
+                        writer.println(output + "~number");
+                        digits.clear();
+                        digit = false;
+                    }
+                    
+                    for (int j = 0; j < other1.length(); j++) {
+                        char temp = other1.charAt(j);
+                        if (temp == ' ') {
+                            continue;
+                        }
+                        boolean isLetter = Character.isLetter(temp);
+                        boolean isDigit = Character.isDigit(temp);
+                        if (isLetter) {
+                            chars.add(temp);
+                            character = true;
+                        } else if (isDigit) {
+                            character = false;
+                            digit = true;
+                            digits.add(temp);
+                        } else {
+                            if (character) {
+                                StringBuilder result = new StringBuilder(chars.size());
+                                for (Character c : chars) {
+                                    result.append(c);
+                                }
+                                String output = result.toString();
+                                identifier.add(output);
+                                boolean res = false;
+                                for (i = 0; i < 8; i++) {
+                                    if (output.equals(reservedWords[i])) {
+                                        res = true;
+                                    }
+                                }
+                                if (res) {
+                                    writer.println(output + "~Reserved_Word");
+                                } else {
+                                    writer.println(output + "~Identifier");
+                                }
+                                chars.clear();
+                                character = false;
+                            } else if (digit) {
+                                StringBuilder result = new StringBuilder(digits.size());
+                                for (Character c : digits) {
+                                    result.append(c);
+                                }
+                                String output = result.toString();
+                                number.add(output);
+                                writer.println(output + "~number");
+                                digits.clear();
+                                digit = false;
+                            }
+                            
+                            special_symbols.add(temp + "");
+                            writer.println(temp + "~Special_Symbol");
+                            
+                        }
+
+                    }
+
+                }
+                   if (character) {
+                        StringBuilder result = new StringBuilder(chars.size());
+                        for (Character c : chars) {
+                            result.append(c);
+                        }
+                        String output = result.toString();
+                        boolean res = false;
+                        for (i = 0; i < 8; i++) {
+                            if (output.equals(reservedWords[i])) {
+                                res = true;
+                            }
+                        }
+                        if (res) {
+                            writer.println(output + "~Reserved_Word");
+                        } else {
+                            writer.println(output + "~Identifier");
+                        }
+                        identifier.add(output);
+                        chars.clear();
+                        character = false;
+                    }
+                    
+                    if (digit) {
+                        StringBuilder result = new StringBuilder(digits.size());
+                        for (Character c : digits) {
+                            result.append(c);
+                        }
+                        String output = result.toString();
+                        number.add(output);
+                        writer.println(output + "~number");
+                        digits.clear();
+                        digit = false;
+                    }
+                        writer.close();
+                    
+            }
+
+        } catch (Exception e) {
+
+        }
+  
+    }
+
+}
